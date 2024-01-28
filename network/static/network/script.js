d6c123845a629c88402329bce4b4323c35d55e8f
Abdalla Diaai <56742529+abdalla-diaai@users.noreply.github.com>
2024-01-28 22:40:32 +0000

    added javascript.

--- network/static/network/script.js
+++ network/static/network/script.js
@@ -1,7 +1,9 @@
 function updateLikes(post_id) {
     fetch(`/allposts/${post_id}`)
+
         .then(response => response.json())
         .then(data => {
             data = JSON.parse(JSON.stringify(data));
+            console.log(data)
             document.querySelector('.likes').textContent(data.likes)}
         )}
